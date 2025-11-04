6553b095fcfd6857f7366135ea389b01c01a8f72
Pattararnxx <pattaraporn.po@kkumail.com>
2025-11-04 14:24:43 +0700

    tools page no response

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
+// import type { NextConfig } from "next";
+//
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+//
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com', // ดึง api ภายนอกมาใช้ ค่อยลบ
+        pathname: '/img/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
