3e888bfac4532764827bf41a84399079e5cd5769
Pattararnxx <pattaraporn.po@kkumail.com>
2025-11-09 14:06:23 +0700

    discover container but no effect

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,21 @@ import Guide from "./components/Guide/guide";
 import Accordion from "@/app/components/Accordion/Accordion";
 import WelcomeText from "./components/Welcome_text/Welcome_text";
 import Toast from "./components/Toast/Toast";
+import Discover from "@/app/components/Discover/discover";
 function page() {
   return (
     <div className={styles.landing}>
-      <Navbar></Navbar>
-      <WelcomeText></WelcomeText>
-      <Category></Category>
-      <Guide></Guide>
-      <Accordion></Accordion>
-      <Toast
-        Variant="success"
-        Title="Success to update"
-        Description=" ReferenceError: Toast is not defined"
-      ></Toast>
+        <Navbar></Navbar>
+        <WelcomeText></WelcomeText>
+        <Category></Category>
+        <Guide></Guide>
+        <Discover></Discover>
+        <Accordion></Accordion>
+        <Toast
+          Variant="success"
+          Title="Success to update"
+          Description=" ReferenceError: Toast is not defined"
+        ></Toast>
     </div>
   );
 }
