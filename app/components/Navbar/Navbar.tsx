99e0537bddcc2136362ac3d7f39e7e3a86cf956a
8hourssleep <phupha.a@kkumail.com>
2025-11-12 23:54:54 +0700

    nav bar for 480px width case

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,9 +1,13 @@
+'use client'
 import React from "react";
+import {useState} from 'react';
 import styles from "./Navbar.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
 function Navbar() {
+  const [sidenavOpen, setSidenavOpen] = useState(false)
+  
   return (
     <div className={styles.navbar}>
       <div className={styles.navbar_inner}>
@@ -36,8 +40,47 @@ function Navbar() {
           >
           </Image>
         </Link>
+        <div className={styles.sidenav_button}
+          onClick={() => setSidenavOpen(!sidenavOpen)}
+        >
+          <Image
+            width={100}
+            height={100}
+            alt="search_icon"
+            src={"/hamburger.svg"}
+            className={styles.hamburger}
+          >
+          </Image>
+        </div>
         <div className={styles.action_button}>let's start</div>
       </div>
+
+        {/* hidden side bar */}
+        <div className={styles.sidenav}  style={{right:`${sidenavOpen ?  '0' : '-100%'}`}}>
+            <div className={styles.sidenav_closed_button}>
+              <Image
+                width={16.4}
+                height={16.4}
+                alt="search_icon"
+                src={"/cross.svg"}
+                className={styles.sidenav_cross}
+                onClick={() => setSidenavOpen(!sidenavOpen)}
+              ></Image>
+            </div>
+            <div className={styles.sidenav_link}>
+              <Link href="/tools">
+                <p>Tools</p>
+              </Link>
+              <p>About</p>
+              <p>Report</p>
+          </div>
+          <div className={styles.sidenav_action_button}>Let's Start</div>
+        </div>
+        { sidenavOpen ? (
+          <div className={styles.sidenav_overlay} onClick={() => setSidenavOpen(!sidenavOpen)}></div>
+        ) : (
+          <></>
+        )}         
     </div>
   );
 }
