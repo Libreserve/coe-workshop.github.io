5c15c8bc651b1a66a0c543feab4769f086492940
8hourssleep <phupha.a@kkumail.com>
2025-11-16 16:13:03 +0700

    avatargroup to display tools image

---
+++ app/components/AvataGroup/AvatarGroup.tsx
@@ -0,0 +1,34 @@
+import styles from "@/app/components/AvataGroup/AvatarGroup.module.scss"
+import Image from "next/image"
+
+function AvataGroup({max, imageList}:AvatarGroup) {
+    return (
+        <>
+            <div className={styles.avatar_group}>
+                {imageList.map((image, index) => {
+                    if (index < max) return (
+                    <div className={styles.avatar} key={`avatar-${index}`}>
+                        <Image
+                            src={image}
+                            alt={`tool-${index}`}
+                            width={100}
+                            height={100}
+                            className={styles.avatar_image}
+                            >
+                        </Image>            
+                    </div>
+                    )
+                    else if (index === max) return (
+                    <div className={styles.avatar} key={`avatar-${index}`}>
+                        <div className={styles.avatar_rest}>
+                                +   {max - index + 1}
+                        </div>
+                    </div>
+                    )
+                    return null;
+                })}
+            </div>
+        </>
+    );
+};
+export default AvataGroup;
