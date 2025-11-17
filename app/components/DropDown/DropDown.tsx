a59a915ec2ce52a03739ee83c5681fa847458694
8hourssleep <phupha.a@kkumail.com>
2025-11-17 16:09:40 +0700

    corrects returned value

--- app/components/DropDown/DropDown.tsx
+++ app/components/DropDown/DropDown.tsx
@@ -1,43 +1,49 @@
-'use client'
-import {use, useEffect, useState} from "react";
-import styles from "./DropDown.module.scss"
+"use client";
+import {useState } from "react";
+import styles from "./DropDown.module.scss";
 import Image from "next/image";
-function DropDown({value, onChange}:DropDown) {
-    const [active, setActive] = useState(false);
-    const [selected, setSelected] = useState("");
-    const states = ["รออนุมัติ", "ระหว่างใช้", "ปฎิเสธ", "คืนแล้ว"]
-    return (
-        <>
-            <div className={styles.dropdown}>
-                <button className={styles.link} onClick={() => {setActive(!active);}}>
-                    <div>
-                        {value || "Pick Value"}
-                    </div>
-                    <Image
-                        src={"arrow2.svg"}
-                        alt={""}
-                        width={20}
-                        height={20}
-                    >
-                    </Image>
-                </button >
-                <div className={`${styles.dropdown_menu}${ active ? "_active" : ""}`}>
-                    {
-                        states.map((state, index) => { return(
-                            <div 
-                            key={`${index}-item`} 
-                            className={`${styles.item}${state === value ? "_selected" : ""}`} 
-                            onClick={() => {setSelected(state); onChange?.(state)}}
-                            > 
-                            {state} 
-                            </div>
-                        )})
-                    }
-                </div>
-                
-            </div>  
-        </>
-    );
-};
+function DropDown({ value, onChange }: DropDown) {
+  const [active, setActive] = useState(false);
+  const [selected, setSelected] = useState<Status | string>("");
+  const states:Status[] = ["pending", "doing", "rejected", "returned"];
+  const getStateInThai:Record<Status, string> = {
+        pending: "รออนุมัติ",
+        doing: "ระหว่างใช้",
+        rejected: "ปฎิเสธ",
+        returned: "คืนแล้ว",
+    }
+  return (
+    <>
+      <div className={styles.dropdown}>
+        <button
+          className={styles.link}
+            onClick={() => {
+            setActive(!active);
+          }}
+        >
+          <div>{ getStateInThai[selected as Status] || "Pick Value"}</div>
+          <Image src={"arrow2.svg"} alt={""} width={20} height={20}></Image>
+        </button>
+        <div className={`${styles.dropdown_menu}${active ? "_active" : ""}`}>
+          {states.map((state, index) => {
+            return (
+              <div
+                key={`${index}-item`}
+                className={`${styles.item}${state === value ? "_selected" : ""}`}
+                onClick={() => {
+                  setSelected(state);
+                  onChange?.(state);
+                  setActive(!active);
+                }}
+              >
+                {getStateInThai[state as Status]}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default DropDown;
