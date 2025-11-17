d8dcdf94ccd24c37235631696df385ad2791bfea
8hourssleep <phupha.a@kkumail.com>
2025-11-17 23:53:38 +0700

    finish implement history page ui, api and filter left

--- app/components/AllHistory/AllHistory.tsx
+++ app/components/AllHistory/AllHistory.tsx
@@ -1,59 +1,109 @@
-'use client'
+"use client";
 import HistoryCard from "../HistoryCard/HistoryCard";
+import DropDown from "../DropDown/DropDown";
+import DatePicker from "../Datepicker/Datepicker";
 import { useState } from "react";
-
-const imageList = [
-    "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
-    "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-    "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_t.png",
-    "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-    "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
-    "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
-    "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
-]
+import styles from "../AllHistory/AllHistory.module.scss";
 
 const mock_from_api = {
-  "transactions": [
+  email: "kritsada.ba@kkumail.com",
+  transactions: [
     {
-      "email": "kritsada.ba@kkumail.com",
-      "toolList": [
-        { "name": "Arduino", "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png", "quantity": 5 },
-        { "name": "Hammer", "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png", "quantity": 1 }
+      toolList: [
+        {
+          name: "Arduino",
+          image:
+            "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
+          quantity: 5,
+        },
+        {
+          name: "Hammer",
+          image:
+            "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
+          quantity: 1,
+        },
       ],
-      "status": "pending",
-      "startDay": "2025-11-17T10:00:00.000Z"
+      status: "pending",
+      startDay: "2025-11-17T10:00:00.000Z",
     },
     {
-      "email": "kritsada.ba@kkumail.com",
-      "toolList": [
-        { "name": "Saw", "image": "/saw.png", "quantity": 2 }
-      ],
-      "status": "returned",
-      "startDay": "2025-11-15T14:30:00.000Z"
-    }
-  ]
-}
-
-const emai = "kritsada.ba@kkumail.com"
-const transaction:Transaction = {
-    toolList: [
-        { "name": "Arduino", "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png", "quantity": 5 },
-        { "name": "Hammer", "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png", "quantity": 1 },
-        { "name": "Hammer", "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png", "quantity": 1 }
-      ],
+      toolList: [{ name: "Saw", image: "/saw.png", quantity: 2 }],
       status: "returned",
-      startDay: "2025-11-17T10:00:00.000Z"
-}
-
+      startDay: "2025-11-15T14:30:00.000Z",
+    },
+    {
+      toolList: [
+      {
+        name: "Arduino",
+        image:
+          "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+        quantity: 5,
+      },
+      {
+        name: "Hammer",
+        image:
+          "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+        quantity: 1,
+      },
+      {
+        name: "Hammer",
+        image:
+          "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+        quantity: 1,
+      },
+      {
+        name: "Hammer",
+        image:
+          "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+        quantity: 1,
+      },
+      {
+        name: "Hammer",
+        image:
+          "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+        quantity: 1,
+      },
+    ],
+    status: "returned",
+    startDay: "2025-11-16T10:00:00.000Z",
+    }
+  ],
+};
 
 function AllHistory() {
-    const [open, setOpen] = useState<boolean>(false);
-    return(
-        <>
-            <HistoryCard setShowHiddenTimestamp={setOpen} showHiddenTimestamp={open} email={emai} transaction={transaction}></HistoryCard>
-        </>
-
-    );
-};
+  const [status, setStatus] = useState<Status | string>("");
+  const email = mock_from_api.email;
+  return (
+    <div className={styles.container}>
+      <div className={styles.table_hearder}>
+        <div className={styles.header}>
+          <h1 >
+            History
+          </h1>
+          <p className={styles.sub_title}>
+            Duis aute irure dolor in reprehenderit.
+          </p>
+        </div>
+        <div className={styles.filter}>
+          <div className={styles.datepicker}>
+            <DatePicker></DatePicker>
+          </div>
+          <div className={styles.status_filter}>
+            <DropDown></DropDown>
+          </div>  
+        </div>
+      </div>
+      <div className={styles.table_body}>
+        {
+          mock_from_api.transactions.map((transaction, index)=> {
+            return (
+              <HistoryCard  transaction={transaction as Transaction}  email={email} key={`${index}`}></HistoryCard>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
 
 export default AllHistory;
--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,43 +1,13 @@
-'use client'
-import DatePicker from "../components/Datepicker/Datepicker";
-import DropDown from "../components/DropDown/DropDown";
-import AvatarGroup from "../components/AvataGroup/AvatarGroup";
-import {useEffect, useState} from "react";
-import TruncateTools from "../components/TruncateTools/TruncateTools";
 import AllHistory from "../components/AllHistory/AllHistory";
-
-
+import Navbar from "../components/Navbar/Navbar";
+import styles from "@/app/page.module.scss";
 
 function history() {
-    const [selectedDate, setSelectedDate] = useState<Date | string |  undefined>("");
-    const [what, setwhat] = useState('I herer')
     return (
-        <>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-            {/* <TruncateTools items={items}></TruncateTools> */}
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
-        <div></div>
-        <div></div>
-        <div style={{ display:'flex'}} >
-            what
-            {/* <DropDown value={what} onChange={(value) => {setwhat(value); console.log(value); console.log("what", what)}}></DropDown> */}
-            {/* <AvatarGroup></AvatarGroup> */}
-            {/* <AvatarGroup max={4} imageList={imageList}></AvatarGroup> */}
-            {/* {result} */}
-            <span>what the helll</span>
-        </div >
-            <span>what the helll</span>
+        <div className={styles.landing}>
+            <Navbar></Navbar>
             <AllHistory></AllHistory>
-        </>
-        
+        </div>    
     );
 }
 
