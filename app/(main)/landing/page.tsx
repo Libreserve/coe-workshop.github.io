8170f1d219eb29163289ee1aeb296d11d42865a7
kitsada babthong <kritsada.ba@kkumail.com>
2026-02-21 23:32:22 +0700

    page not found

--- app/(main)/landing/page.tsx
+++ app/(main)/landing/page.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import LandingClient from "./LandingClient";
 
-export const Page = () => {
+const Page = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <LandingClient />
     </Suspense>
   );
 };
+
+export default Page;
--- app/(main)/tools/page.tsx
+++ app/(main)/tools/page.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import ToolsClient from "./ToolClient";
 
-export const Page = () => {
+const Page = () => {
   return (
     <Suspense fallback={<div>loading...</div>}>
       <ToolsClient></ToolsClient>
     </Suspense>
   );
 };
+
+export default Page;
